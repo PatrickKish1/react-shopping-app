b6fc71136cd6d9b8ecf819faa293ce429e02518e
PatrickKish1 <patrickkesh90@gmail.com>
2024-06-23 18:10:07 -0800

    Updating files

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ItemCard = ({ item, addToCart }) => {
   return (
     <div className="card">
-      <img src={item.image} className="card-img-top" alt="..." />
+      <img src={item.image} className="card-img-top" alt="....." />
       <div className="card-body">
         <h4 className="card-title text-center">{item.name}</h4>
         <p className="card-text text-center">Price: ${item.price}</p>
