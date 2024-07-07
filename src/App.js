05873621d73d0b1469881038f4e4784e45a90287
Patrick <98470284+PatrickKish1@users.noreply.github.com>
2024-07-07 06:06:01 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ const items = [
   },
   {
       id: 9,
-      name: 'Web Development E-Book',
+      name: 'Web Dev E-Book',
       image: 'https://images.unsplash.com/photo-1507721999472-8ed4421c4af2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       price: 10
   },
